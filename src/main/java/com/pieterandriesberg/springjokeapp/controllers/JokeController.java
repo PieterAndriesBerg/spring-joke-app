da7cb0a6d69b451fd9dce6c5289df2360f8a6cf0
PieterAndriesBerg <p-a.berg@outlook.com>
2018-12-07 10:47:14 +0100

    added JokeController

---
+++ src/main/java/com/pieterandriesberg/springjokeapp/controllers/JokeController.java
@@ -0,0 +1,29 @@
+package com.pieterandriesberg.springjokeapp.controllers;
+
+import com.pieterandriesberg.springjokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by pieterandries on 7-12-18
+ **/
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
