729e510b0ba5bfa4933faa1aefda4e9fa4432c2e
PieterAndriesBerg <p-a.berg@outlook.com>
2018-12-07 10:35:34 +0100

    initial commit pushing project to github

---
+++ src/main/java/com/pieterandriesberg/springjokeapp/SpringJokeAppApplication.java
@@ -0,0 +1,12 @@
+package com.pieterandriesberg.springjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokeAppApplication.class, args);
+    }
+}
